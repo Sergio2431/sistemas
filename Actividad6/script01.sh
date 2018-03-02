98c86b613fb3dc8a72eb5cc35c34e29970ab6f0a
root <root@sergio-VirtualBox.(none)>
2018-03-02 11:45:22 +0100

    subir archivos

--- Actividad6/script01.sh
+++ Actividad6/script01.sh
@@ -3,7 +3,7 @@ read -p"Introduce un numero Y" y
 
 if [ $x -gt $y ]; then
     echo "el numero $x es el mayor"
-else if [ $y -gt $y ]; then
+else if [ $y -gt $x ]; then
     echo "el numero $y es el mayor"
 else 
     echo "Los numeros son iguales"
