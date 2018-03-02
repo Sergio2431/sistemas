8476495d8012096a758d9a72ae77c633f1fd122e
root <root@sergio-VirtualBox.(none)>
2018-03-02 12:46:52 +0100

    subir archivos

---
+++ Actividad6/script05.sh
@@ -0,0 +1,11 @@
+read -p "Introduce un numero: " x
+
+suma=0
+contador=0
+
+while [ $x -ne 0 ]; do
+    suma=$((suma + x))
+    contador=$((contador + 1))
+    read -p "Introduce un número: " x
+done
+echo"Has introducido suma de $contador con un valor medio de $media"
