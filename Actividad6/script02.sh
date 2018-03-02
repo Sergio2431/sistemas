95d7cb181370792a9c0e78da5d1d95105ba3da92
root <root@sergio-VirtualBox.(none)>
2018-03-02 12:00:47 +0100

    subir archivos

--- Actividad6/script02.sh
+++ Actividad6/script02.sh
@@ -1,1 +1,11 @@
-read -p "Escribe un numero mayor que 0"
+read -p "Escribe un número mayor que 0: " x
+
+while[ $x -le 0]; do
+    read -p "Escribe un número mayor que 0: " x
+done
+
+if[ `expr $x %2´ -eq 0]; then
+    echo "El número $x es par"
+else
+    echo "El número $x es impar"
+fi
